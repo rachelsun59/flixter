c9da4ac0909c7415cef6f812c6f9ff3e15c63ebf
Rachel Sun <rachel.yufei.sun@gmail.com>
2019-08-31 10:38:52 -0400

    Listing the courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,7 @@ class Course < ApplicationRecord
 end
 class Course < ApplicationRecord
     belongs_to :user
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
   end
-  

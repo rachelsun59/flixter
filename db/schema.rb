40826d66ba8c7b4ca403be2e818dc39012d68f3d
Rachel Sun <rachel.yufei.sun@gmail.com>
2019-09-07 15:54:16 -0400

    Install Image Magick

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_171241) do
+ActiveRecord::Schema.define(version: 2019_09_07_183045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_171241) do
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "lessons", force: :cascade do |t|
